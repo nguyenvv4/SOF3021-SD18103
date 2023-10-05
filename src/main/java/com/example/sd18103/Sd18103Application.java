55bff868bb1c39e63aa47cf0b0aa244cf5931a52
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2023-10-05 16:54:57 +0700

    update

--- src/main/java/com/example/sd18103/Sd18103Application.java
+++ src/main/java/com/example/sd18103/Sd18103Application.java
@@ -3,8 +3,10 @@ package com.example.sd18103;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Sd18103Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/sd18103/controller/ReportController.java
+++ src/main/java/com/example/sd18103/controller/ReportController.java
@@ -4,12 +4,14 @@ import com.example.sd18103.model.Report;
 import com.example.sd18103.model.SinhVien;
 import com.example.sd18103.repository.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RequestMapping("/report")
@@ -24,5 +26,15 @@ public class ReportController {
         List<Report> list = reportRepository.getAll();
         list.forEach(x -> System.out.println(x.toString()));
         return null;
+    }
+
+    @Scheduled(fixedDelay = 3000)
+    public void test(){
+        System.out.println(new Date());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
